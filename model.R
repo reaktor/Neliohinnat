2e1b84e192e19701e0809d757fd83c460c5fc161
euxoa <janne.sinkkonen@gmail.com>
2014-12-09 15:50:01 +0200

    Update model.R

--- model.R
+++ model.R
@@ -1,4 +1,4 @@
-setwd("~/Desktop/Fonecta/Neliohinnat")
+library(lme4)
 
 d <- read.table("sane-ashi.txt", sep=" ", header=T, colClasses=list(pnro="factor"))
 d <- transform(d, 
@@ -7,6 +7,7 @@ d <- transform(d,
              level3=as.factor(substr(pnro, 1, 1)), 
              yr=year-2013)
 
+# zip level trend does not fit with lme4. 
 m <- lmer(log(price) ~ yr  + (1|pnro) + (1+yr|level1) + (1+yr|level2) + (1|level3), data=d, weights=d$n)
 
 zero.na <- function (x) { x[is.na(x)]=0; x}
@@ -23,6 +24,7 @@ tpred <- function (m, pnro) {
     zero.na(ranef(m)$level2[substr(pnro, 1, 2), 2]) 
 }
 
+# Oops, FIXME.
 d.pono <- read.csv("../Ruutudata/rttk2012_250_ponot.csv", sep=";", header=T, colClasses="character")
 pnrot <- unique(d.pono$PNRO)
 write.table(data.frame(pnro=pnrot, hintataso=exp(pred(m, pnrot)), trendi=tpred(m, pnrot))[
