3c186f9009ca22fca0945ad7331b3a85a1707fb9
Jaakko Särelä <jaakko.sarela@reaktor.fi>
2014-12-10 13:26:36 +0200

    väestöntiheyden ja hintatrendin suhde

---
+++ tiheys_trendit.R
@@ -0,0 +1,36 @@
+# Script for analysing the relation between population density and price trends
+library("ggplot2")
+
+load("pnro_spatial_epsg2393.RData")
+
+population <- read.csv("vakiluku_posnro_2012_proper.csv", sep=";", header=T, colClasses="character",row.names=1)
+
+# Read hinta data
+pnro.hinnat <- read.table("pnro-hinnat.txt", sep=" ", header=TRUE, colClasses="character")
+pnro.hinnat$hintataso <- as.numeric(pnro.hinnat$hintataso)
+pnro.hinnat$trendi <- as.numeric(pnro.hinnat$trendi)
+
+
+population$vakiluku = as.numeric(population$vakiluku)
+
+population$logtiheys = NaN
+
+rows <- row.names(population)
+# add population density
+for (index in seq(length(pnro.sp.alt@data$pnro))) {
+  if (pnro.sp.alt@data$pnro[index] %in% rows){
+    if (population[pnro.sp.alt@data$pnro[index],]$vakiluku > 0) {
+    population[pnro.sp.alt@data$pnro[index],]$logtiheys = log(population[pnro.sp.alt@data$pnro[index],]$vakiluku / pnro.sp.alt@data$area.m2[index])
+}}}
+
+population$trendi = NaN
+
+# add population trendi
+for (index in seq(length(pnro.sp.alt@data$pnro))) {
+  population[pnro.sp.alt@data$pnro[index],]$trendi = pnro.hinnat$trendi[index]
+}
+
+#png("trendi_tiheys.png", width=2000, height=3000)
+ggplot(population, aes(x=logtiheys,y=trendi)) + geom_point(shape=1) + geom_smooth(method="gam", formula = y ~ s(x))
+ggsave("trendi_tiheys.png")
+dev.off()
