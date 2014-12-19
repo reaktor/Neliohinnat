9b88573d347d809278b1afae51898e2c9cc1ec8d
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2014-12-19 17:40:49 +0200

    Further code cleanup.

--- source/model-stan2.R
+++ source/model-stan2.R
@@ -3,8 +3,8 @@ library(rstan)
 
 # FIXME: Reduce()
 first.nna2 <- function (c1, c2)  ifelse(!is.na(c1), c1, c2)
-first.nna4 <- function (c1, c2, c3, c4) first.nna2(c1, first.nna2(c2, first.nna2(c3, c4)))
-
+first.nna <- function (...) Reduce(first.nna2, list(...))
+  
 prefix.factor <- function (pnro, n) as.factor(substr(pnro, 1, n))
 l3 <- { . %>% prefix.factor(., 1) }
 l2 <- { . %>% prefix.factor(., 2) }
@@ -39,45 +39,49 @@ s <- readRDS("s.rds")
 # Note that it would be better to compute several chains.
 # You should monitor the convergence here somehow.
 # Those tau's are notoriously well autocorrelated.
-s
-traceplot(s, "Omega", inc_warmup=F, ask=T)
-traceplot(s, "Omega3", inc_warmup=F, ask=T)
-traceplot(s, "tau1", inc_warmup=F)
-traceplot(s, "tau3", inc_warmup=F)
-# etc.
+if (F) {
+  s
+  traceplot(s, "Omega", inc_warmup=F, ask=T)
+  traceplot(s, "Omega3", inc_warmup=F, ask=T)
+  traceplot(s, "tau1", inc_warmup=F)
+  traceplot(s, "tau3", inc_warmup=F)
+  # etc.
+}
+
+# Low-level correlation matrix over price level, trend, etc.
+# Is of general interest
+Omega <- apply(extract(s, "Omega")[[1]], c(2, 3), median)
+saveRDS(Omega, "data/Omega.rds")
 
 beta.prm.mean <- function (v) apply(extract(s, v)[[1]], c(2, 3), mean)
-# For debugging
-beta <- beta.prm.mean("beta")
-lhinta <- beta[,1]+6
-trendi <- beta[,2]/10
-quad <- beta[,3]
+# For debugging and watching around
+if (F) {
+  beta <- beta.prm.mean("beta")
+  lhinta <- beta[,1]+6
+  trendi <- beta[,2]/10
+  quad <- beta[,3]
+}
 
 par.tbl <- function(d, v.name, b.name, name.postfix) 
   data.frame(levels(d[[v.name]]), beta.prm.mean(b.name)) %>% 
   setNames(c(v.name, paste(c("lhinta", "trendi", "quad"), name.postfix, sep=""))) %>%
   tbl_df()
 
+# Span the space on pnro's form the spatial data.
+pnro <- local({
+  load("data/pnro_spatial_wgs84.RData") # And pnro.sp magically appears (FIXME: rds...)
+  pnro.sp$pnro }) 
 
-# For NA pnro's, look for upper level in the hierarchy and take beta1
-load("data/pnro_spatial_wgs84.RData")
-pnro <- pnro.sp$pnro
-pnro.pars <- par.tbl(d, "pnro", "beta", "")
-level1.pars <- par.tbl(d, "level1", "beta1", "1")
-level2.pars <- par.tbl(d, "level2", "beta2", "2")
-level3.pars <- par.tbl(d, "level3", "beta3", "3")
-res <- data.frame(pnro, 
-                  level1 = l1(pnro), 
-                  level2 = l2(pnro), 
-                  level3 = l3(pnro)) %>% 
-  left_join(pnro.pars, by="pnro") %>% 
-  left_join(level1.pars, by="level1") %>% 
-  left_join(level2.pars, by="level2") %>% 
-  left_join(level3.pars, by="level3") %>% 
+# For NA pnro's in the model, look for upper level in the hierarchy and take beta1 etc.
+res <- data.frame(pnro, level1 = l1(pnro), level2 = l2(pnro), level3 = l3(pnro)) %>% 
+  left_join(par.tbl(d, "pnro", "beta", ""), by="pnro") %>% 
+  left_join(par.tbl(d, "level1", "beta1", "1"), by="level1") %>% 
+  left_join(par.tbl(d, "level2", "beta2", "2"), by="level2") %>% 
+  left_join(par.tbl(d, "level3", "beta3", "3"), by="level3") %>% 
   transmute(pnro=pnro, 
-            lhinta=first.nna4(lhinta, lhinta1, lhinta2, lhinta3), 
-            trendi=first.nna4(trendi, trendi1, trendi2, trendi3), 
-            quad=first.nna4(quad, quad1, quad2, quad3)) %>%
+            lhinta=first.nna(lhinta, lhinta1, lhinta2, lhinta3), 
+            trendi=first.nna(trendi, trendi1, trendi2, trendi3), 
+            quad=first.nna(quad, quad1, quad2, quad3)) %>%
   mutate(hintataso=exp(lhinta))
 
 write.table(res,  "data/pnro-hinnat.txt", row.names=F, quote=F)
