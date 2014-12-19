036e053dbfcfd12f20aeff18445807650d5e98e9
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2014-12-19 17:57:25 +0200

    Removed polygon borders.

--- source/pnro_plot.R
+++ source/pnro_plot.R
@@ -7,8 +7,8 @@ pnro.hinnat <- readRDS("data/pnro-hinnat.rds")
 load("data/pnro_spatial_epsg2393.RData") # pnro.sp.alt appears here
 pnro.hinnat.sp <- merge(pnro.sp.alt, pnro.hinnat)
 pdf("figs/pnro_prices.pdf")
-spplot(pnro.hinnat.sp, zcol="lhinta", lwd=0.01, main="log.price")
-spplot(pnro.hinnat.sp, zcol="trendi", lwd=0.01, main="trend")
-spplot(pnro.hinnat.sp, zcol="quad", lwd=0.01, main="quadratic")
+spplot(pnro.hinnat.sp, zcol="lhinta", lwd=0.00, col="transparent", main="log.price")
+spplot(pnro.hinnat.sp, zcol="trendi", lwd=0.00, col="transparent", main="trend")
+spplot(pnro.hinnat.sp, zcol="quad", lwd=0.00, col="transparent", main="quadratic")
 dev.off()
 
