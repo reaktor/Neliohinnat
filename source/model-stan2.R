b43f278a82ac6b2c408fc57d690eed6de8105dc6
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2014-12-19 17:02:10 +0200

    Code cleanup

--- source/model-stan2.R
+++ source/model-stan2.R
@@ -1,18 +1,27 @@
 library(dplyr)
 library(rstan)
 
+# FIXME: Reduce()
+first.nna2 <- function (c1, c2)  ifelse(!is.na(c1), c1, c2)
+first.nna4 <- function (c1, c2, c3, c4) first.nna2(c1, first.nna2(c2, first.nna2(c3, c4)))
+
+prefix.factor <- function (pnro, n) as.factor(substr(pnro, 1, n))
+l3 <- { . %>% prefix.factor(., 1) }
+l2 <- { . %>% prefix.factor(., 2) }
+l1 <- { . %>% prefix.factor(., 3) }
+
 d <- readRDS("data/statfi_ashi_pnro_processed_2005-2014_20141219.rds") %>% 
   tbl_df() %>% 
   filter(!is.na(price)) %>%
-  mutate(pnro=as.factor(as.character(pnro)), year=as.numeric(as.character(year))) %>%
-  mutate(level1 = as.factor(substr(pnro, 1, 3)),
-         level2 = as.factor(substr(pnro, 1, 2)),
-         level3 = as.factor(substr(pnro, 1, 1)), 
+  mutate(pnro=factor(pnro), year=as.numeric(as.character(year))) %>% # pnro has extra levels
+  mutate(level1 = l1(pnro),
+         level2 = l2(pnro),
+         level3 = l3(pnro), 
          yr = (year-mean(year))/10,
          lprice = log(price)-6)
 
 
-wtf <- function (d, cl, cu) { d1 <- unique(data.frame(l=as.numeric(d[,cl][[1]]), u=as.numeric(d[,cu][[1]]))); d1[order(d1$l),]$u }
+wtf <- function (d, cl, cu) data.frame(l=as.numeric(d[[cl]]), u=as.numeric(d[[cu]])) %>% unique %>% { .[order(.$l),]$u }
 
 #m <- lmer(log(price) ~ yr  + (1|pnro) + (1+yr|level1) + (1+yr|level2) + (1|level3), data=d, weights=d$n)
 m <- stan_model(file="source/m2.stan")
@@ -29,45 +38,38 @@ s <- readRDS("s.rds")
 
 # Note that it would be better to compute several chains.
 # You should monitor the convergence here somehow.
+# Those tau's are notoriously well autocorrelated.
 s
 traceplot(s, "Omega", inc_warmup=F, ask=T)
 traceplot(s, "Omega3", inc_warmup=F, ask=T)
 traceplot(s, "tau1", inc_warmup=F)
 traceplot(s, "tau3", inc_warmup=F)
+# etc.
 
-beta <- apply(extract(s, "beta")[[1]], c(2, 3), mean)
-beta1 <- apply(extract(s, "beta1")[[1]], c(2, 3), mean)
-beta2 <- apply(extract(s, "beta2")[[1]], c(2, 3), mean)
-beta3 <- apply(extract(s, "beta3")[[1]], c(2, 3), mean)
+beta.prm.mean <- function (v) apply(extract(s, v)[[1]], c(2, 3), mean)
+# For debugging
+beta <- beta.prm.mean("beta")
 lhinta <- beta[,1]+6
 trendi <- beta[,2]/10
 quad <- beta[,3]
-lhinta1 <- beta1[,1]+6
-trendi1 <- beta1[,2]/10
-quad1 <- beta1[,3]
-lhinta2 <- beta2[,1]+6
-trendi2 <- beta2[,2]/10
-quad2 <- beta2[,3]
-lhinta3 <- beta3[,1]+6
-trendi3 <- beta3[,2]/10
-quad3 <- beta3[,3]
-# hist(trendi, n=100)
-# plot(beta[,1], trendi)
 
-first.nna2 <- function (c1, c2)  ifelse(!is.na(c1), c1, c2)
-first.nna4 <- function (c1, c2, c3, c4) first.nna2(c1, first.nna2(c2, first.nna2(c3, c4)))
+par.tbl <- function(d, v.name, b.name, name.postfix) 
+  data.frame(levels(d[[v.name]]), beta.prm.mean(b.name)) %>% 
+  setNames(c(v.name, paste(c("lhinta", "trendi", "quad"), name.postfix, sep=""))) %>%
+  tbl_df()
+
 
 # For NA pnro's, look for upper level in the hierarchy and take beta1
 load("data/pnro_spatial_wgs84.RData")
 pnro <- pnro.sp$pnro
-pnro.pars <- data.frame(pnro=levels(d$pnro), lhinta, trendi, quad)
-level1.pars <- data.frame(level1=levels(d$level1), lhinta1, trendi1, quad1)
-level2.pars <- data.frame(level2=levels(d$level2), lhinta2, trendi2, quad2)
-level3.pars <- data.frame(level3=levels(d$level3), lhinta3, trendi3, quad3)
+pnro.pars <- par.tbl(d, "pnro", "beta", "")
+level1.pars <- par.tbl(d, "level1", "beta1", "1")
+level2.pars <- par.tbl(d, "level2", "beta2", "2")
+level3.pars <- par.tbl(d, "level3", "beta3", "3")
 res <- data.frame(pnro, 
-                  level1 = as.factor(substr(pnro, 1, 3)), 
-                  level2 = as.factor(substr(pnro, 1, 2)), 
-                  level3 = as.factor(substr(pnro, 1, 1))) %>% 
+                  level1 = l1(pnro), 
+                  level2 = l2(pnro), 
+                  level3 = l3(pnro)) %>% 
   left_join(pnro.pars, by="pnro") %>% 
   left_join(level1.pars, by="level1") %>% 
   left_join(level2.pars, by="level2") %>% 
--- source/pnro_plot.R
+++ source/pnro_plot.R
@@ -4,11 +4,11 @@
 pnro.hinnat <- readRDS("data/pnro-hinnat.rds")
 
 ## Plot with alternative coordinates
-load("data/pnro_spatial_epsg2393.RData")
+load("data/pnro_spatial_epsg2393.RData") # pnro.sp.alt appears here
 pnro.hinnat.sp <- merge(pnro.sp.alt, pnro.hinnat)
 pdf("figs/pnro_prices.pdf")
 spplot(pnro.hinnat.sp, zcol="lhinta", lwd=0.01, main="log.price")
 spplot(pnro.hinnat.sp, zcol="trendi", lwd=0.01, main="trend")
-spplot(pnro.hinnat.sp, zcol="quad", lwd=0.01, main="qudratic")
+spplot(pnro.hinnat.sp, zcol="quad", lwd=0.01, main="quadratic")
 dev.off()
 
