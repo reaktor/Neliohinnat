60e18c2301e0f7b329c7d6bd9f4e8ff41c8cab8f
ouzor <ouzor@L690.local>
2015-01-20 11:11:41 +0200

    Removed -Inf values from pnro_prices.geojson

--- source/pnro_geojson.R
+++ source/pnro_geojson.R
@@ -7,10 +7,30 @@ pnro.hinnat <- readRDS("data/pnro-hinnat.rds")
 load("data/pnro_spatial_wgs84.RData") # pnro.sp.alt appears here
 pnro.hinnat.sp <- merge(pnro.sp, pnro.hinnat)
 
-# Take only Helsinki as an example
+# Write only polygons as GeoJSON (can not specify file type for some reason, rename afterwards)
+rgdal::writeOGR(obj=pnro.sp, dsn="pnro_areas_geojson", layer="postinumerot 20140217", driver="GeoJSON")
+file.rename("pnro_areas_geojson", "pnro_areas.geojson")
 
+
+# Remove -Inf values
+infs <- unique(unlist(lapply(pnro.hinnat.sp@data, function(x) which(x==Inf))))
+pnro.hinnat.sp <- pnro.hinnat.sp[-infs, ]
 
 # Write as GeoJSON (can not specify file type for some reason, rename afterwards)
 rgdal::writeOGR(obj=pnro.hinnat.sp, dsn="pnro_prices_geojson", layer="postinumerot 20140217", driver="GeoJSON")
 file.rename("pnro_prices_geojson", "pnro_prices.geojson")
 # validate with http://geojsonlint.com/
+
+# Take only Helsinki as an example
+pnro.hinnat.pks.sp <- pnro.hinnat.sp[substr(pnro.hinnat.sp$pnro, 1, 2) %in% c("00", "01", "02"), ]
+
+rgdal::writeOGR(obj=pnro.hinnat.pks.sp, dsn="pnro_pks_prices_geojson", layer="postinumerot 20140217", driver="GeoJSON")
+file.rename("pnro_pks_prices_geojson", "pnro_pks_prices.geojson")
+
+
+# Try as gist
+library("devtools")
+install_github("ropensci/rgbif")
+library("rgbif")
+?gist("")
+
