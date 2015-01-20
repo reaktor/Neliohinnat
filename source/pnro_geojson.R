3323150d0e9524967d2e676a913207d423fb3dd7
ouzor <ouzor@L690.local>
2015-01-20 10:34:08 +0200

    Added price info to pnro_prices.geojson

--- source/pnro_geojson.R
+++ source/pnro_geojson.R
@@ -8,6 +8,6 @@ load("data/pnro_spatial_wgs84.RData") # pnro.sp.alt appears here
 pnro.hinnat.sp <- merge(pnro.sp, pnro.hinnat)
 
 # Write as GeoJSON (can not specify file type for some reason, rename afterwards)
-rgdal::writeOGR(obj=pnro.sp, dsn="pnro_prices_geojson", layer="postinumerot 20140217", driver="GeoJSON")
+rgdal::writeOGR(obj=pnro.hinnat.sp, dsn="pnro_prices_geojson", layer="postinumerot 20140217", driver="GeoJSON")
 file.rename("pnro_prices_geojson", "pnro_prices.geojson")
 # validate with http://geojsonlint.com/
