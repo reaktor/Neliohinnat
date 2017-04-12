ce24812f43d89b206c548fc65344a1c073290e2e
ouzor <ouzor@MacBook-Pro-2.local>
2017-04-12 15:32:12 +0300

    update run stan script to run 10k samples for short and long data

--- source_2017/run_stan_model_2017.R
+++ source_2017/run_stan_model_2017.R
@@ -23,11 +23,11 @@ d <- pnro.ashi.dat %>%
          yr = year2yr(year),
          lprice = log(price)-6)
 
-saveRDS(d, "data_2017/d.rds")
+saveRDS(d, "data_2017/d_20170412.rds")
 
 ## RUN STAN #########
 
-d <- readRDS("data_2017/d.rds")
+d <- readRDS("data_2017/d_20170412.rds")
 # wtf <- function (d, cl, cu) data.frame(l=as.numeric(d[[cl]]), u=as.numeric(d[[cu]])) %>% unique %>% { .[order(.$l),]$u }
 
 m <- stan_model(file="source_2017/model_2017.stan")
@@ -53,37 +53,58 @@ s <- s.f(4, iter=50, warmup=5, thin=1, refresh=1)
 
 ## Run in AWS ############
 
+# # Run eight long chains for final results
+# s <- s.f(nchains=8, iter=2000, warmup=1000, thin=20, refresh=50)
+# saveRDS(s, "data_2017/model_samples_debug_8chains_1000+1000t20_20170406.rds")
+
 # Run eight long chains for final results
-s <- s.f(nchains=8, iter=2000, warmup=1000, thin=20, refresh=50)
-saveRDS(s, "data_2017/model_samples_debug_8chains_1000+1000t20_20170406.rds")
+s_long <- s.f(nchains=8, iter=10000, warmup=5000, thin=100, refresh=50)
+saveRDS(s_long, "data_2017/long_model_samples_8chains_5000+5000t100_20170412.rds")
 
+## Run shorter model for years 2010-2016 shorter years ################
 
-## Alternative, run with years 2010-2016 ###########
-
-load("data_2017/pnro_data_20170405.RData")
-d <- pnro.ashi.dat %>%
-  filter(year >= 2010) %>%
-  # Compute NEGATIVE log.density because Janne
-  mutate(log.density = -log(density_per_km2)/10) %>%
-  #  mutate(log.density = (log(density_per_km2)-14)/10) # This would be close to d$log.density
-  filter(!is.na(log.density) & !is.na(price)) %>%
-  mutate(pnro=factor(pnro), year=as.numeric(as.character(year))) %>% # pnro has extra levels
-  mutate(level1 = l1(pnro),
-         level2 = l2(pnro),
-         level3 = l3(pnro), 
-         yr = year2yr(year),
-         lprice = log(price)-6)
-saveRDS(d, "data_2017/d_2010-2016.rds")
-d <- readRDS("data_2017/d_2010-2016.rds")
-m <- stan_model(file="source_2017/model_2017.stan")
-s.f = function (nchains=1, iter=2500, warmup=1000, thin=25, refresh=-1)
-  sampling(m, data=with(d, list(N=nrow(d), M=nlevels(pnro), M1=nlevels(level1), M2=nlevels(level2), 
+d_short <- d %>% filter(year >= 2010)
+s.f_short <- function (nchains=1, iter=2500, warmup=1000, thin=25, refresh=-1)
+  sampling(m, data=with(d_short, list(N=nrow(d_short), M=nlevels(pnro), M1=nlevels(level1), M2=nlevels(level2), 
                                 lprice=lprice, count=n, yr=yr, z=log.density,
                                 pnro=as.numeric(pnro), 
                                 l1=as.numeric(level1),
                                 l2=as.numeric(level2))), 
            iter=iter, warmup=warmup, thin=thin, init=0, chains=nchains, cores= nchains, refresh=refresh, seed=4)
 
+s_short <- s.f_short(4, iter=50, warmup=5, thin=1, refresh=1)
+
 # Run eight long chains for final results
-s <- s.f(nchains=8, iter=2000, warmup=1000, thin=20, refresh=50)
-saveRDS(s, "data_2017/model_2010-2016_samples_debug_8chains_1000+1000t20_20170407.rds")
+s_short <- s.f_short(nchains=8, iter=10000, warmup=5000, thin=100, refresh=50)
+saveRDS(s_short, "data_2017/short_model_samples_8chains_5000+5000t100_20170412.rds")
+
+
+# ## Alternative, run with years 2010-2016 ###########
+# 
+# load("data_2017/pnro_data_20170405.RData")
+# d <- pnro.ashi.dat %>%
+#   filter(year >= 2010) %>%
+#   # Compute NEGATIVE log.density because Janne
+#   mutate(log.density = -log(density_per_km2)/10) %>%
+#   #  mutate(log.density = (log(density_per_km2)-14)/10) # This would be close to d$log.density
+#   filter(!is.na(log.density) & !is.na(price)) %>%
+#   mutate(pnro=factor(pnro), year=as.numeric(as.character(year))) %>% # pnro has extra levels
+#   mutate(level1 = l1(pnro),
+#          level2 = l2(pnro),
+#          level3 = l3(pnro), 
+#          yr = year2yr(year),
+#          lprice = log(price)-6)
+# saveRDS(d, "data_2017/d_2010-2016.rds")
+# d <- readRDS("data_2017/d_2010-2016.rds")
+# m <- stan_model(file="source_2017/model_2017.stan")
+# s.f = function (nchains=1, iter=2500, warmup=1000, thin=25, refresh=-1)
+#   sampling(m, data=with(d, list(N=nrow(d), M=nlevels(pnro), M1=nlevels(level1), M2=nlevels(level2), 
+#                                 lprice=lprice, count=n, yr=yr, z=log.density,
+#                                 pnro=as.numeric(pnro), 
+#                                 l1=as.numeric(level1),
+#                                 l2=as.numeric(level2))), 
+#            iter=iter, warmup=warmup, thin=thin, init=0, chains=nchains, cores= nchains, refresh=refresh, seed=4)
+# 
+# # Run eight long chains for final results
+# s <- s.f(nchains=8, iter=2000, warmup=1000, thin=20, refresh=50)
+# saveRDS(s, "data_2017/model_2010-2016_samples_debug_8chains_1000+1000t20_20170407.rds")
